2736ad94f2e83e116f2ce0f1dc7726bb9a3ec80b
Petro Lesapiti <petrolesapiti@gmail.com>
2023-11-23 12:14:02 +0300

    Create desktop sidebar

---
+++ components/sidebar.tsx
@@ -0,0 +1,93 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Montserrat } from "next/font/google";
+import {
+  LayoutDashboard,
+  MessageSquare,
+  ImageIcon,
+  VideoIcon,
+  Music,
+  Code,
+  Settings,
+} from "lucide-react";
+
+import { cn } from "@/lib/utils";
+const montserrat = Montserrat({
+  weight: "600",
+  subsets: ["latin"],
+});
+const routes = [
+  {
+    label: "Dashbord",
+    icon: LayoutDashboard,
+    href: "/dashboard",
+    color: "text-sky-500",
+  },
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    href: "/conversation",
+    color: "text-violet-500",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    href: "/image",
+    color: "text-pink-700",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    href: "/video",
+    color: "text-orange-700",
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    href: "/music",
+    color: "text-emerald-500",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    href: "/code",
+    color: "text-green-700",
+  },
+  {
+    label: "Settings",
+    icon: Settings,
+    href: "/setting",
+  },
+];
+const Sidebar = () => {
+  return (
+    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+      <div className="px-3 py-2 flex-1">
+        <Link href="/dashboard" className="flex items-center mb-14 pl-3">
+          <div className="mr-4 h-8 w-8 relative">
+            <Image src="/logo.png" alt="Logo Image" fill />
+          </div>
+          <h1 className={cn("text-2xl font-bold", montserrat.className)}>
+            Taasio
+          </h1>
+        </Link>
+        <div>
+          {routes.map((route) => (
+            <Link
+              href={route.href}
+              key={route.href}
+              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={(cn("h-5 w-5 mr-3"), route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
