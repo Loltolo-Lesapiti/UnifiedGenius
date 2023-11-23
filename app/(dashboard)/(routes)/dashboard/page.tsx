e7445a4a8ed5682401c9f54143bd1ec5396fb14d
Petro Lesapiti <petrolesapiti@gmail.com>
2023-11-23 12:18:19 +0300

    Update routes

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-import React from "react";
-
 const DashboardPage = () => {
   return (
     <div className="flex justify-between">
       <h3 className="text-2xl text-slate-500">DashboardPage(Protected)</h3>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
