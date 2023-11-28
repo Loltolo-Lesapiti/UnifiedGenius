89cb26ad9693ca966038d9249f6469a0058214de
Petro Lesapiti <petrolesapiti@gmail.com>
2023-11-28 12:15:05 +0300

    create navigation

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,13 @@
+"use client";
 import React from "react";
-import { Button } from "./ui/button";
-import { Menu } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
+
+import MobileSidebar from "./mobile_sidebar";
 
 const Navbar = () => {
   return (
     <div className="flex items-center p-4">
-      <Button className="md:hidden" variant="ghost" size="icon">
-        <Menu />
-      </Button>
+      <MobileSidebar />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>
